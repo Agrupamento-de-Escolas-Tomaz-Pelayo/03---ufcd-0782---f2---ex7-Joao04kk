b9658d583e0a13d92306c40869c6f0b421505a77
Joao04kk <a32423@tomazpelayo.com>
2023-02-01 14:52:48 +0000

    ex7

--- LetrasOla.c
+++ LetrasOla.c
@@ -1,3 +1,13 @@
 // Resolva neste ficheiro o seu exercício
+#include <stdio.h>
 
+int main()
+{
+    printf(" ####  #        ##\n");
+    printf("#    # #       #  #\n");
+    printf("#    # #      ######\n");
+    printf(" ####  ##### #      #\n");
+    return(0);
+
+}
 
